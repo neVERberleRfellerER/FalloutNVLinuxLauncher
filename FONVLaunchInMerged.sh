2e2cf36fc39d3e43f9a2107fc220c78d41ece2f6
Ondrej Kraus <neverberlerfellerer@gmail.com>
2019-01-08 12:24:32 +0100

    First public version

---
+++ FONVLaunchInMerged.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+set -e
+
+ROOTDIR="/home/user/.wine/drive_c/Fallout"
+MODDIR="$ROOTDIR/mods"
+
+FONVDIR="$ROOTDIR/Fallout New Vegas_clean"
+MERGERDIR="$ROOTDIR/Fallout New Vegas"
+MODDATADIR="$MODDIR/data"
+OVFSWORKDIR="$MODDIR/_work"
+OVFSOVERLAYDIR="$MODDIR/_overlay"
+
+BASEORDER=$(cat <<'EOF'
+FalloutNV.esm
+DeadMoney.esm
+HonestHearts.esm
+OldWorldBlues.esm
+LonesomeRoad.esm
+GunRunnersArsenal.esm
+Fallout3.esm
+Anchorage.esm
+ThePitt.esm
+BrokenSteel.esm
+PointLookout.esm
+Zeta.esm
+CaravanPack.esm
+ClassicPack.esm
+MercenaryPack.esm
+TribalPack.esm
+TaleOfTwoWastelands.esm
+YUPTTW.esm
+EOF
+)
+
+if [ "$#" -eq 0 ]; then
+	programname=`basename "$0"`
+	echo "ERROR: No program supplied"
+	echo
+	echo "Usage: $programname <program-to-run>"
+	exit 1
+fi
+
+LOWERDIRS=`find "$MODDATADIR" -mindepth 1 -maxdepth 1 -type d -print0 | sort -z | tr '\0' ':' | sed 's/:$//'`
+
+echo "Overlay stacking will be (highest to lowest priority):"
+echo "$(echo $LOWERDIRS | tr ':' '\n')"
+
+# find base game ESPs and ESMs, remove duplicates keepeing last occurence
+GAMEORDER=$(find "$FONVDIR/Data" -mindepth 3 -maxdepth 3 -type f \( -name '*.esp' -or -name '*.esm' \) -print0 | sort -z | xargs -r0 -n1 basename | awk '!seen[$0]++' | tac)
+# find mod ESPs and ESMs, remove duplicates keepeing last occurence
+MODORDER=$(find "$MODDATADIR" -mindepth 3 -maxdepth 3 -type f \( -name '*.esp' -or -name '*.esm' \) -print0 | sort -z | xargs -r0 -n1 basename | awk '!seen[$0]++' | tac)
+
+# remove files with predefined order
+MODORDER=$(grep -Fxvf <(echo "$BASEORDER") <(echo "$MODORDER"))
+# concatenate base and mod lists and reverse result to keep it in same logical
+# order that mod overlays are using
+MODORDER=$(echo -e "$BASEORDER\n$MODORDER" | tac)
+
+echo "Mounting $MERGERDIR"
+echo "If requested, enter password for sudo to mount overlaysfs"
+
+mkdir -p "$OVFSOVERLAYDIR" "$OVFSWORKDIR"
+
+# mount overlay and start unmounter
+WAITFORPID=$BASHPID
+sudo -k -- /bin/sh <<EOF
+# mount overlay
+mount -t overlay overlay -o lowerdir="$LOWERDIRS:$FONVDIR",upperdir="$OVFSOVERLAYDIR",workdir="$OVFSWORKDIR" "$MERGERDIR" || {
+    echo "Overlay mounting failed"
+    exit 1
+}
+# start watcher to unmount when main process dies
+sh -c "echo 'Waiting for $WAITFORPID before unmounting'; tail --pid=$WAITFORPID -f /dev/null; sleep 5; echo 'Unmounting $MERGERDIR'; exec umount -l '$MERGERDIR'" &
+EOF
+
+# change file times to enforce load order - the newer file is, the higher
+# is its priority
+MODCOUNT=$(echo -n "$MODORDER" | grep -c '^')
+idx=1
+while read -r line; do
+    touch -d "$idx days ago" "$MERGERDIR/Data/$line"
+    (( idx++ ))
+done < <(echo "$MODORDER")
+
+# save mod load order into text file to allow symlinking to it
+find "$MERGERDIR"/Data -mindepth 1 -maxdepth 1 -type f \( -name '*.esm' -o -name '*.esp' \) -printf '%Ts\t%p\n' | sort -n | cut -f2 | xargs -n1 -d '\n' basename > "$MERGERDIR"/Data/loadorder.txt
+
+echo "Mod load order is:"
+cat "$MERGERDIR"/Data/loadorder.txt
+echo
+
+echo "Possible mixed-case file conflicts:"
+find "$MERGERDIR" | perl -ne 's!([^/]+)$!lc $1!e; print if 1 == $seen{$_}++'
+echo
+
+exec env ROOTDIR="$ROOTDIR" FONVDIR="$MERGERDIR" "$@"
---
+++ README.md
@@ -0,0 +1,58 @@
+# Example usage
+
+## Variables
+
+FONVDIR - directory with game (originally created for FONV, hence the name). Nothing will be modified in this directory.
+BASEORDER - fixed order of ESM/ESPs at beginning of load order - these will be prepended and order determined by directory scan will be ignored for these. Useful for TTW which contains many files and they have to be interleaved with base game files.
+MODDATADIR - directory that contains mod structured as described in "Directory structure". Nothing will be changed in this directory.
+MERGERDIR - directory where merged filesystem will be mounted
+OVFSWORKDIR - what OverlayFS calls work dir; not important, just point it to writable location
+OVFSOVERLAYDIR - what OverlayFS calls overlay dir; not important, just point it to writable location
+
+All changes (including creation and deletion of files) are handled by OverlayFS so even UIO is perfectly safe to use - it will not change anything in MODDATADIR or FONVDIR.
+
+## Directory structure
+
+Scripts expects clean game directory (tested with Fallout NV) in FONVDIR, mods in MODDATADIR and writable directory in MODDIR where OverlayFS files will be stored.
+
+MODDATADIR should contain directories that after ascending ASCII alphabetical ordering matches mod load order where first directories have highest priority and latest lowest priority. Example:
+
+$MODDATADIR/0001 libvorbis
+$MODDATADIR/0002 native dlls
+$MODDATADIR/0004 enb
+$MODDATADIR/0005 UIO
+$MODDATADIR/0010 JIP LN NVSE
+$MODDATADIR/0495 Vanilla UI Plus
+$MODDATADIR/9500 BLEED
+$MODDATADIR/9800 JIP CCC
+$MODDATADIR/9850 MCM
+$MODDATADIR/9998 TTW321
+$MODDATADIR/9999 TTW32
+
+You can pick whatever ordering you want, but number prefixing is commonly used in world of UNIX-like systems for config ordering.
+
+The resulting mod load order is such that TTW have lovest priority and libvorbis (dll files) highest, effectively overwriting files with same names from TTW.
+
+To ensure proper load order make sure that each directory contains only one 
+
+## Start
+
+FONVDIR variable is provided by FONVLaunchInMerged so you don't have to hardcode mergedi variable at two places.
+
+exec env WINEESYNC=1 WINEDLLOVERRIDES="d3d9=n,b" WINE_LARGE_ADDRESS_AWARE=1 ./FONVLaunchInMerged.sh sh -c 'cd "$FONVDIR"; exec wine FalloutNV.exe'
+
+or
+
+exec env WINEESYNC=1 WINEDLLOVERRIDES="d3d9=n,b" WINE_LARGE_ADDRESS_AWARE=1 ./FONVLaunchInMerged.sh sh -c 'cd "$FONVDIR"; exec wine FalloutNVLauncher.exe'
+
+Put this to script for easier launching.
+
+Since mod authors use variaous file name casing, script will check MODDATADIR for possible conflicts. Conflicts are echoed after "Possible mixed-case file conflicts:" line and the line is always printed even when no conflicts are present - in this case there will be empty line below the message.
+
+Mod will also create
+$MERGERDIR/Data/loadorder.txt
+that you can symlink to AppData/Local/FalloutNV/plugins.txt if necessary.
+
+# License
+
+WTFPL
